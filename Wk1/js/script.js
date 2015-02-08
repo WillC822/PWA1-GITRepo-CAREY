d40a8e351ed6894adc939339139792e0fc233a05
DOC <WDCarey@fullsail.edu>
2015-02-08 12:47:11 -0500

    Setup Players Variable

--- Wk1/js/script.js
+++ Wk1/js/script.js
@@ -1,3 +1,28 @@
 /**
  * Created by DoC822 on 2/8/15.
  */
+/*
+William Carey
+2.8.2015
+Assignment Goal1: Assignment: Duel1
+ */
+
+// Self Executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Dr Octopus";
+
+    //player damage
+    var playerOneDamage = 20;
+    var playerTwoDamage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    
+})();

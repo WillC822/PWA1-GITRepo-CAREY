d30a18c8b909544d6b14ac0ee52aa844208a76d5
DOC <WDCarey@fullsail.edu>
2015-02-08 13:24:03 -0500

    added function and for loop

--- Wk1/js/script.js
+++ Wk1/js/script.js
@@ -17,12 +17,46 @@ Assignment Goal1: Assignment: Duel1
     var playerTwoName = "Dr Octopus";
 
     //player damage
-    var playerOneDamage = 20;
-    var playerTwoDamage = 20;
+    var player1Damage = 20;
+    var player2Damage = 20;
 
     //player health
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
-    
+    //rounds
+    var round = 1;
+
+    function fight(){
+        //code will go here
+        console.log('in the fight function');
+
+        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+
+        for(var i=0; i< 10; i++){
+
+            //random formula is - Math.floor(Math.random() * (max - min) + min);
+
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            console.log(f1);
+            console.log(f2);
+
+
+
+        };
+    };
+
+    function winnerCheck(){
+        //code will go here
+    };
+
+    /************ program starts here*************/
+    console.log('program starts');
+    fight();
+
+
 })();

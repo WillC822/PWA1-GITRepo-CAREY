fab0f4269c117a80bb524e8cde678240ae898a9d
DOC <WDCarey@fullsail.edu>
2015-02-09 12:59:47 -0500

    finished duel with alerts to show winner

--- Wk1/js/script.js
+++ Wk1/js/script.js
@@ -59,7 +59,7 @@ Assignment Goal1: Assignment: Duel1
 
             if(results === "no winner"){
                 round++;
-                console.log(playerOneName+":"+playerOneHealth+" *ROUND"+round+" Over* "+playerTwoName+":"+playerTwoHealth);
+                alert(playerOneName+":"+playerOneHealth+" *ROUND"+round+" Over* "+playerTwoName+":"+playerTwoHealth);
             }else{
                 alert(results);
                 break;
@@ -75,10 +75,10 @@ Assignment Goal1: Assignment: Duel1
 
         if(playerOneHealth<1 && playerTwoHealth<1){
             result = "You Both Die";
-        }else if(playerOneHealth<1){
-            result =playerTwoName+" WINS!!!"
-        }else if(playerTwoHealth<1){
-            result =playerOneName+" WINS!!!"
+        }else if(playerOneHealth < 1){
+            result = playerTwoName+" WINS!!!"
+        }else if(playerTwoHealth < 1){
+            result = playerOneName+" WINS!!!"
         };
         //code will go here
         return result;
